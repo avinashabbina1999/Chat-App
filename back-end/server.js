3d0a9966acf47ac827c5fdf6c33c01d1c1348039
avinashabbina <aabbina1s@semo.edu>
2025-04-09 14:25:57 -0500

    added deployment necessities

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+
 import authRouter from './routes/auth.routes.js'
 import connectToDb from './db/connectToDb.js'
 import messagesRouter from './routes/messages.routes.js'
@@ -11,6 +13,8 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -18,6 +22,13 @@ app.use(cookieParser())
 app.use('/api/auth',authRouter)
 app.use('/api/messages',messagesRouter)
 app.use('/api/users',userRouter)
+
+
+app.use(express.static(path.join(__dirname,'/front-end/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'front-end','dist','index.html'))
+})
 
 server.listen(PORT,()=>{
     connectToDb()
